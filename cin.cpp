9923ca58bd3775c02ba56918f06fa67780c27120
Arun Cyriac <76899266+aruncyriac@users.noreply.github.com>
2024-11-15 22:48:26 +0530

    Add files via upload

---
+++ cin.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    char ch;
+    double d;
+    string str;
+    cout << "Enter the values";
+    cin >> num >> ch >> d >> str;
+    cout << "Values are \n";
+    cout << num << endl;
+    cout << ch << "\n";
+    cout << d << "\n";
+    cout << str << std::endl;
+}
---
+++ cout.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num = 12;
+    char ch = 'B';
+    double d = 12.34;
+    string str = "Emertxe";
+    cout << num << endl;
+    cout << ch << "\n";
+    cout << d << "\n";
+    cout << str << std::endl;
+    std::cout << "\n" << d << "\n" << num << "\n";
+    return 0;
+}
